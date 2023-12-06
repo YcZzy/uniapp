e10910fd9448c45db9734f6e9e23344541d7ea0c
Olivia Carter <577814350@qq.com>
2023-12-06 15:44:43 +0800

    Initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { useQuery } from '@tanstack/vue-query'
+import { fetchGitHubRepo } from '@/api'
+
+// #ifdef MP
+const repo = ref('yang1206/uniapp-template')
+const { data } = useQuery(computed(() => {
+  return {
+    ...fetchGitHubRepo(repo.value),
+  }
+}))
+// #endif
+</script>
+
+<template>
+  <div class="px-4 py-10 text-center font-sans text-gray-700">
+    <div>
+      <div class="i-carbon-campsite" inline-block text-80 />
+      <p>
+        <a :href="data?.html_url" text-14px text-gray decoration-none op75 rel="noreferrer" target="_blank">
+          一个uniapp，vite，vue3的起始模版
+        </a>
+      </p>
+      <div class="py-4" />
+      <Counter />
+    </div>
+    <div flex="~ justify-evenly">
+      <image class="logo" src="/static/logo.png" />
+      <image class="logo" src="/static/vue.svg" />
+      <image class="logo" src="/static/vite.svg" />
+    </div>
+  </div>
+</template>
+
+<route  lang="json">
+{
+  "style": {
+    "navigationStyle": "default",
+    "navigationBarTitleText": "Home"
+  }
+}
+</route>
+
+<style scoped>
+.logo {
+  width: 100rpx;
+  height: 100rpx;
+  margin-top: 200rpx;
+  margin-bottom: 50rpx;
+  text-decoration: none;
+}
+</style>
